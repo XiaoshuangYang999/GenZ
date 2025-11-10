3a67ab62c21386f430a4fa4715f766738ed22612
Xiaoshuang <134080799+XiaoshuangYang999@users.noreply.github.com>
2025-11-10 13:25:19 +0100

    add lwb-s4 executable

---
+++ exec/lwb-s4.hs
@@ -0,0 +1,61 @@
+module Main where
+
+import System.Timeout
+import Control.Exception
+
+import General
+import FormM.Lex
+import FormM.Parse
+
+-- TODO import multiple logics, qualified, add command line arg to choose it
+import S4
+
+main :: IO ()
+main = do
+  -- Set a time limit.
+  -- Test cases will be discarded if they take more than 5 seconds.
+  let limit = 5 * 1000000 -- in microseconds
+  -- content <- readFile "benchmarks/LWB/lwb_k/k_dum_p.txt.1.intohylo"--benchmark/lwb_k/
+  -- putStrLn "k_dum_p 1"
+  -- case parseFormM (alexScanTokens content) of
+  --   Left e -> print e
+  --   Right f -> do
+  --     r <- timeout limit (evaluate $ isProvableZ k (negM f))
+  --     case r of
+  --       Nothing -> putStrLn "Timeout"
+  --       Just b  -> print b
+  -- content2 <- readFile "benchmarks/LWB/lwb_k/k_dum_n.txt.1.intohylo"--benchmark/lwb_k/
+  -- putStrLn "k_dum_n 1"
+  -- case parseFormM (alexScanTokens content2) of
+  --   Left e -> print e
+  --   Right f -> do
+  --     r <- timeout limit (evaluate $ isProvableZ k (negM f))
+  --     case r of
+  --       Nothing -> putStrLn "Timeout"
+  --       Just b  -> print b
+  -- putStrLn "Give me a formula: "
+  -- l <- getLine
+  -- print l
+  -- case parseFormM (alexScanTokens l) of
+  --   Left e -> print e
+  --   Right f -> do
+  --     print f
+  --     print $ isProvableZ k f
+  putStrLn "Give me the formula's name: "
+  name <- getLine
+  putStrLn "Give me the size of the formula: "
+  size <- getLine
+  content <- readFile $ "benchmarks/LWB/lwb_s4/" ++ name ++ ".txt." ++ size ++ ".intohylo"
+  case parseFormM (alexScanTokens content) of
+    Left e -> print e
+    Right f -> do
+      putStr "GenZ: "
+      r <- timeout limit (evaluate $ isProvableZ sfour (negM f))
+      case r of
+        Nothing -> putStrLn "Timeout"
+        Just b  -> print b
+      putStr "GenT: "
+      r2 <- timeout limit (evaluate $ isProvableT sfour (negM f))
+      case r2 of
+        Nothing -> putStrLn "Timeout"
+        Just b  -> print b
