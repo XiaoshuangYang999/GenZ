8c313a32a947555da0af842b6e3396a00e0ff0d1
Xiaoshuang <134080799+XiaoshuangYang999@users.noreply.github.com>
2025-11-10 12:37:29 +0100

    input name +size output result

--- exec/genz.hs
+++ exec/genz.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import General
+import System.Timeout
+import Control.Exception
 
+import General
 import FormM.Lex
 import FormM.Parse
 
@@ -10,11 +12,50 @@ import K
 
 main :: IO ()
 main = do
-  putStrLn "Give me a formula: "
-  l <- getLine
-  print l
-  case parseFormM (alexScanTokens l) of
+  -- Set a time limit.
+  -- Test cases will be discarded if they take more than 5 seconds.
+  let limit = 5 * 1000000 -- in microseconds
+  -- content <- readFile "benchmarks/LWB/lwb_k/k_dum_p.txt.1.intohylo"--benchmark/lwb_k/
+  -- putStrLn "k_dum_p 1"
+  -- case parseFormM (alexScanTokens content) of
+  --   Left e -> print e
+  --   Right f -> do
+  --     r <- timeout limit (evaluate $ isProvableZ k (negM f))
+  --     case r of
+  --       Nothing -> putStrLn "Timeout"
+  --       Just b  -> print b
+  -- content2 <- readFile "benchmarks/LWB/lwb_k/k_dum_n.txt.1.intohylo"--benchmark/lwb_k/
+  -- putStrLn "k_dum_n 1"
+  -- case parseFormM (alexScanTokens content2) of
+  --   Left e -> print e
+  --   Right f -> do
+  --     r <- timeout limit (evaluate $ isProvableZ k (negM f))
+  --     case r of
+  --       Nothing -> putStrLn "Timeout"
+  --       Just b  -> print b
+  -- putStrLn "Give me a formula: "
+  -- l <- getLine
+  -- print l
+  -- case parseFormM (alexScanTokens l) of
+  --   Left e -> print e
+  --   Right f -> do
+  --     print f
+  --     print $ isProvableZ k f
+  putStrLn "Give me the formula's name: "
+  name <- getLine
+  putStrLn "Give me the size of the formula: "
+  size <- getLine
+  content <- readFile $ "benchmarks/LWB/lwb_k/" ++ name ++ ".txt." ++ size ++ ".intohylo"
+  case parseFormM (alexScanTokens content) of
     Left e -> print e
     Right f -> do
-      print f
-      print $ isProvableZ k f
+      putStr "GenZ: "
+      r <- timeout limit (evaluate $ isProvableZ k (negM f))
+      case r of
+        Nothing -> putStrLn "Timeout"
+        Just b  -> print b
+      putStr "GenT: "
+      r2 <- timeout limit (evaluate $ isProvableT k (negM f))
+      case r2 of
+        Nothing -> putStrLn "Timeout"
+        Just b  -> print b
