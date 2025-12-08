b33b569982f4445fb0f9bb36a59139b71ee35eda
Malvin Gattinger <malvin@w4eg.de>
2025-12-08 20:07:51 +0100

    cli: all logics, more options

--- exec/genz.hs
+++ exec/genz.hs
@@ -1,48 +1,125 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
-import System.Timeout
-import Control.Exception
-import System.Environment (getArgs)
+import Options.Applicative
 
 import General
 import General.Lex
-import FormM.Parse
+import General.Token
 
-import Logic.K as K
-import Logic.S4 as S4
+import FormM
+import FormM.Parse
+import FormP
+import FormP.Parse
+
+import Logic.CPL
+import Logic.IPL
+import Logic.D
+import Logic.D4
+import Logic.GL
+import Logic.K
+import Logic.K4
+import Logic.S4
+import Logic.T
+
+data Input = FileInput FilePath | DirectInput String | StdInput
+
+data Config = Config
+  { input :: Input
+  , tree :: Bool
+  , proof :: Bool
+  , log :: MyLogic }
 
 main :: IO ()
-main = do
-  (input,options) <- getInputAndSettings
-  let myIsProvable = if "--tree" `elem` options then isProvableT else isProvableZ
-  let myLogic = if "--s4" `elem` options then sfour else k
-  let limit = 20 * 1000000 -- in microseconds
-  case parseFormM (alexScanTokens input) of
-    Left e -> putStrLn $ "Parse error at" ++ show e
-    Right f -> do
-      r <- timeout limit (evaluate $ myIsProvable myLogic (neg f))
-      case r of
-        Nothing -> putStrLn "Timeout"
-        Just b  -> print b
+main = runGenZ =<< execParser opts where
+  opts = info (configP <**> helper) (fullDesc <> progDesc "Prove the given FORMULA or the formula in FILE or STDIN."
+    <> header "genz - a generic sequent calculus prover with zippers")
 
-getInputAndSettings :: IO (String,[String])
-getInputAndSettings = do
-  args <- getArgs
-  case args of
-    ("-":options) -> do
-      input <- getContents
-      return (input,options)
-    (filename:options) -> do
-      input <- readFile filename
-      return (input,options)
-    _ -> do
-      {-
-      name <- getProgName
-      mapM_ (hPutStrLn stderr)
-        [ infoline
-        , "usage: " ++ name ++ " <filename> {options}"
-        , "       (use filename - for STDIN)\n"
-        , "  -tex   generate LaTeX code\n"
-        , "  -show  write to /tmp, generate PDF and show it (implies -tex)\n" ]
-      -}
-      return ("",[])
+runGenZ :: Config -> IO ()
+runGenZ (Config inp useTree showProof myL) = do
+  form_s <- case inp of FileInput file  -> readFile file -- TODO: ignore "begin" and "end" here or in Lexer/Parser?
+                        DirectInput f_s -> return f_s
+                        StdInput        -> getContents -- TODO same
+  let tl = case myLex form_s of Left errs -> error $ unlines errs
+                                Right tl_ -> tl_
+  putStrLn $ case myL of
+    Left l_prop ->
+      case myParse form_s tl parseFormP of
+        Left errs -> error (unlines errs)
+        Right f_prop -> prChoose useTree showProof l_prop f_prop
+    Right l_mod ->
+      case myParse form_s tl parseFormM of
+        Left errs -> error (unlines errs)
+        Right f_mod -> prChoose useTree showProof l_mod f_mod
+
+-- | Helper to chooe tree/zipper and bool/proof output
+prChoose :: (Show f, Ord f) => Bool -> Bool -> Logic f -> (f -> String)
+prChoose True  True  l = show . proveT l
+prChoose True  False l = show . isProvableT l
+prChoose False True  l = show . proveZ l
+prChoose False False l = show . isProvableZ l
+
+configP :: Parser Config
+configP = Config
+      <$> inputP
+      <*> Options.Applicative.switch
+          ( long "tree"
+         <> short 't'
+         <> help "Use standard trees (default is to use zippers)." )
+      <*> Options.Applicative.switch
+          ( long "proof"
+         <> short 'p'
+         <> help "Print the (partial) proof (default is only True/False)." )
+      <*> option (maybeReader logicR)
+          ( long "logic"
+         <> short 'l'
+         <> help "Logic to use: CPL, IPL, D, D4, GL, K, K4, S4, T"
+         <> showDefaultWith (\ case Left l -> name l; Right l -> name l)
+         <> value (Right Logic.K.k)
+         <> metavar "LOGIC" )
+
+type MyLogic = (Either (Logic FormP) (Logic FormM))
+
+logicR :: String -> Maybe MyLogic
+logicR l_s = case l_s of
+  "CPL" -> return $ Left Logic.CPL.classical
+  "IPL" -> return $ Left Logic.IPL.intui
+  "D"   -> return $ Right Logic.D.d
+  "D4"  -> return $ Right Logic.D4.dfour
+  "GL"  -> return $ Right Logic.GL.gl
+  "K"   -> return $ Right Logic.K.k
+  "K4"  -> return $ Right Logic.K4.kfour
+  "S4"  -> return $ Right Logic.S4.sfour
+  "T"   -> return $ Right Logic.T.t
+  _ -> error $ "Unknown logic: " ++ l_s
+
+fileInputP :: Parser Input
+fileInputP = FileInput <$> strOption (long "file" <> short 'f' <> metavar "FILE" <> help "Input file" )
+
+directInputP :: Parser Input
+directInputP = DirectInput <$> strOption (long "formula" <> short 'F' <> metavar "FORMULA" <> help "Formula" )
+
+stdInputP :: Parser Input
+stdInputP = flag' StdInput (long "stdin" <> short 's' <> help "Read from stdin" )
+
+inputP :: Parser Input
+inputP = directInputP <|> fileInputP <|> stdInputP
+
+myLex :: String -> Either [String] [Token AlexPosn]
+myLex textinput = do
+  let lexResult = alexScanTokensSafe textinput
+  case lexResult of
+    Left (line,col) -> Left
+      [ "\nINPUT: " ++ textinput
+      , replicate (col + length ("INPUT:" :: String)) ' ' ++ "^"
+      , "Lexing error in line " ++ show line ++ " column " ++ show col ++ "." ]
+    Right tokenList -> Right tokenList
+
+myParse :: String -> [Token AlexPosn] -> ([Token AlexPosn] -> Either (Int,Int) f) -> Either [String] f
+myParse textinput tl myParser = case myParser tl of
+  Left (line,col) -> Left
+    [ "\nINPUT: " ++ textinput
+    , replicate (col + length ("INPUT:" :: String)) ' ' ++ "^"
+    , "Parse error in line " ++ show line ++ " column " ++ show col ]
+  Right frm -> Right frm
