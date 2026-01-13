99eb96339643bb105adbd52667bbefb3d5ba4348
Malvin Gattinger <malvin@w4eg.de>
2026-01-13 16:35:19 +0100

    add flag to negate input formula

--- exec/genz.hs
+++ exec/genz.hs
@@ -28,6 +28,7 @@ data Input = FileInput FilePath | DirectInput String | StdInput
 data Config = Config
   { input :: Input
   , tree :: Bool
+  , negate :: Bool
   , proof :: ProofFormat
   , log :: MyLogic }
 
@@ -37,7 +38,7 @@ main = runGenZ =<< execParser opts where
     <> header "genz - a generic sequent calculus prover with zippers")
 
 runGenZ :: Config -> IO ()
-runGenZ (Config inp useTree prFormat myL) = do
+runGenZ (Config inp useTree negate prFormat myL) = do
   form_s <- case inp of FileInput file  -> readFile file -- TODO: ignore "begin" and "end" here or in Lexer/Parser?
                         DirectInput f_s -> return f_s
                         StdInput        -> getContents -- TODO same
@@ -47,11 +48,13 @@ runGenZ (Config inp useTree prFormat myL) = do
     Left l_prop ->
       case myParse form_s tl parseFormP of
         Left errs -> error (unlines errs)
-        Right f_prop -> prChoose useTree prFormat l_prop f_prop
+        Right f_prop -> prChoose useTree prFormat l_prop
+          ((if negate then neg else id) f_prop)
     Right l_mod ->
       case myParse form_s tl parseFormM of
         Left errs -> error (unlines errs)
-        Right f_mod -> prChoose useTree prFormat l_mod f_mod
+        Right f_mod -> prChoose useTree prFormat l_mod
+          ((if negate then neg else id) f_mod)
 
 -- | Helper to chooe tree/zipper and bool/proof output
 prChoose :: (Show f, Ord f, TeX f) => Bool -> ProofFormat -> Logic f -> (f -> String)
@@ -69,6 +72,10 @@ configP = Config
           ( long "tree"
          <> short 't'
          <> help "Use standard trees (default is to use zippers)." )
+      <*> Options.Applicative.switch
+          ( long "negate"
+         <> short 'n'
+         <> help "Negate the input formula." )
       <*> option (maybeReader outputR)
           ( long "proofFormat"
          <> short 'p'
