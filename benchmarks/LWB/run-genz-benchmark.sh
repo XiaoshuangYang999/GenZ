b196411a83a3c684cd561eee0e48744f7ff1aca3
Malvin Gattinger <malvin@w4eg.de>
2026-01-20 16:44:41 +0100

    bench script: 300s timeout, debug, also run tree version

--- benchmarks/LWB/run-genz-benchmark.sh
+++ benchmarks/LWB/run-genz-benchmark.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
 
+# TODO: check that genz is actually on the PATH
+
+mkdir -p logs
+mkdir -p treelogs
+
+echo "Now running zipper benchmarks."
+
 for i in `ls lwb_k -Sr`; do
     echo $i
-    time (timeout 60s genz -n -l K -f lwb_k/$i > logs/${i}_output.log) &> logs/${i}_time.log
+    time (timeout 300s genz -d -n -l K -f lwb_k/$i > logs/${i}_output.log) &> logs/${i}_time.log
 done
+
+echo "Now running tree benchmarks."
+
+for i in `ls lwb_k -Sr`; do
+    echo $i
+    time (timeout 300s genz -t -d -n -l K -f lwb_k/$i > treelogs/${i}_output.log) &> treelogs/${i}_time.log
+done
+
+echo "Done."
